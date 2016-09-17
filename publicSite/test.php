054331c9c739b90e85c8618d18be3a2910d78363
RadOctocode <happylife345@gmail.com>
2016-09-17 06:22:02 -0400

    commit

---
+++ publicSite/test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to ClassM8s</title>
+  </head>
+  <body>
+    <div >
+      <h1>Welcome to ClassM8s</h1>
+      <p>the php is working!</p>
+      <ul>
+        <li><a href="questions.html">test me</a></li>
+        <?php
+          echo 'php is working'
+          ?>
+      </ul>
+    </div>
+  </body>
+</html>
