229de1348027e4c3db80694f19b501f3d2404477
RadOctocode <happylife345@gmail.com>
2016-09-17 18:35:20 -0400

    changes

--- publicSite/local vars.js
+++ publicSite/local vars.js
@@ -1,30 +1,160 @@
 
 function createQuestionnaire(){
-var studentName=document.getElementById("studentName").value;
+
 var que1=document.getElementById("que1").value;
 var que2=document.getElementById("que2").value;
-var que3=document.getElementById("que3").value;
 
-document.getElementById("studentName").value = "";
-document.getElementById("que1").value = "";
-document.getElementById("que2").value = "";
-document.getElementById("que3").value = "";
 
- $.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=MATH", success: function(result){
+function course(aSubject,aCatalogNum,aTitle,aLevel){
+		this.aSubject =aSubject;
+		this.aCatalogNum =aCatalogNum;
+		this.aTitle =aTitle;
+		this.aLevel =aLevel;
+		this.aScore;
+		this.hasCredit =false;
+}
+
+
+
+$.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=MATH", success: function(result){
         var oMath = result;
-        var arrayMathClass=[];
+		var coursesMath =[];
+		for(i = 0; i<oMath.data.classes.length; i++){
+			if(oMath.data.classes[i].catalogNbr == 1910 || oMath.data.classes[i].catalogNbr == 1920 || oMath.data.classes[i].catalogNbr == 2930 ||  oMath.data.classes[i].catalogNbr == 2940  )
+				coursesMath.push(new course(oMath.data.classes[i].subject, oMath.data.classes[i].catalogNbr, oMath.data.classes[i].titleLong, oMath.data.classes[i].catalogNbr/1000.0 ));
+		}
 
-         console.log("result:");
-        console.log(oMath);
+$.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=PHYS", success: function(result){
+       var oPhys = result;
+       var coursesPhys=[];
+	   for(i = 0; i<oPhys.data.classes.length; i++){
+	   		if(oPhys.data.classes[i].catalogNbr == 1112 || oPhys.data.classes[i].catalogNbr == 1116 || oPhys.data.classes[i].catalogNbr == 2213 || oPhys.data.classes[i].catalogNbr == 2214)
+				coursesPhys.push(new course(oPhys.data.classes[i].subject, oPhys.data.classes[i].catalogNbr, oPhys.data.classes[i].titleLong, oPhys.data.classes[i].catalogNbr/1000.0 ));
+	   }
+        
+$.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=CHEM", success: function(result){
+        var oChem = result;
+        var coursesChem=[];        
+		for(i = 0; i<oChem.data.classes.length; i++)
+			if(oChem.data.classes[i].catalogNbr == 2090 || oChem.data.classes[i].catalogNbr == 2150)
+				coursesChem.push(new course(oChem.data.classes[i].subject, oChem.data.classes[i].catalogNbr, oChem.data.classes[i].titleLong, oChem.data.classes[i].catalogNbr/1000.0 ));
+			coursesChem[0].aLevel =1;
+		
 
-        console.log("result.length:")
-        console.log(oMath.data.classes.length);
+$.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=CS", success: function(result){
+        var oCs = result;
+        var coursesCs=[];
+		for(i = 0; i<oCs.data.classes.length; i++){
+			if(oCs.data.classes[i].catalogNbr == 1110 || oCs.data.classes[i].catalogNbr == 1112 || oCs.data.classes[i].catalogNbr == 2110 || oCs.data.classes[i].catalogNbr == 2112 ||
+				oCs.data.classes[i].catalogNbr == 2850 || oCs.data.classes[i].catalogNbr == 3410 || oCs.data.classes[i].catalogNbr == 3310)
+				coursesCs.push(new course(oCs.data.classes[i].subject, oCs.data.classes[i].catalogNbr, oCs.data.classes[i].titleLong, oCs.data.classes[i].catalogNbr/1000.0 ));
+         }
 
-        for (var i = 0; i < oMath.data.classes.length; i++) {
-        	var nameOfMathClass=oMath.data.classes[i].titleLong;
-        	arrayMathClass.push(nameOfMathClass);
-   
+engriCourse=new course("Engri","1001","EngriCourse","1");
+
+var MathList =[];
+switch(que1){
+
+          case("Mechanical Engineer"):
+          case("Electrical and Computer Engineer"):
+          case("Applied and Engineering Physics"):
+             //All First Year Engineers
+              for(i = 0; i<coursesMath.length; i++){
+                if ((coursesMath[i].aTitle.search("for Engineers") > -1) && (coursesMath[i].aCatalogNum%2000>=1000))
+                	MathList.push(coursesMath[i].aSubject+" "+coursesMath[i].aCatalogNum+" "+coursesMath[i].aTitle);   
+              }
+
+              document.getElementById('printOut').appendChild(makeUL(MathList));
+                console.log(oChem.data.classes[6].subject+" "+oChem.data.classes[6].catalogNbr)
+                break;
+      }
+
+        var credit = new Set();
+     if(document.getElementById('chk1').checked)
+     	credit.add("CHEM2090");
+     if(document.getElementById('chk2').checked)
+     	credit.add("PHYS1112");
+     if(document.getElementById('chk3').checked)
+     	credit.add("PHYS2213");
+     if(document.getElementById('chk4').checked && document.getElementById('chk5'))
+     	credit.add("PHYS1112");
+     if(document.getElementById('chk6').checked)
+     	credit.add("CS1110");
+        
+     if (credit.has(oMath.data.classes[10].aCatalogNum)){ 
+         console.log(oMath.data.classes[11].subject+" "+oMath.data.classes[11].catalogNbr+" "+oMath.data.classes[11].titleLong);     }  
+     else{
+          console.log(oMath.data.classes[10].subject+" "+oMath.data.classes[10].catalogNbr+" "+oMath.data.classes[10].titleLong);
+	}
+            
+        
+      if(credit.has(oCs.data.classes[0].catalogNbr) || credit.has(oCs.data.classes[1].catalogNbr)){
+         for(i = 0; i<41; i++){
+            console.log(engri.data.classes[i].subject+" "+engri.data.classes[i].catalogNbr+" "+engri.data.classes[i].titleLong); //shows ENGRI options if u have CS credit
+       		 }
+            }
+            
+       else{ console.log(oCs.data.classes[0].subject+" "+oCs.data.classes[0].catalogNbr+" "+oCs.data.classes[0].titleLong); //shows CS 1110,1112 if no CS credit
+            console.log(oCs.data.classes[1].subject+" "+oCs.data.classes[1].catalogNbr+" "+oCs.data.classes[1].titleLong);
+     		}
+
+   if(credit.has(oChem.data.classes[6].catalogNbr)){ //chem priority over phys
+       if(credit.has(oPhys.data.classes[2].catalogNbr) && credit.has(oPhys.data.classes[8].catalogNbr)) {
+			console.log("Take a fun class")
+		}
+ 	else if(credit.has(oPhys.data.classes[2].catalogNbr)){
+			console.log(oPhys.data.classes[8].subject+" "+oPhys.data.classes[8].catalogNbr+" "+oPhys.data.classes[8].titleLong);
         }
+     else if(credit.has(oPhys.data.classes[8].catalogNbr)){
+            console.log(oPhys.data.classes[2].subject+" "+oPhys.data.classes[2].catalogNbr+" "+oPhys.data.classes[2].titleLong);
+			console.log(oPhys.data.classes[3].subject+" "+oPhys.data.classes[3].catalogNbr+" "+oPhys.data.classes[3].titleLong); } 
+		}
+
+    else{   console.log(oChem.data.classes[6].subject+" "+oChem.data.classes[6].catalogNbr+" "+oChem.data.classes[6].titleLong);
+            console.log(oChem.data.classes[7].subject+" "+oChem.data.classes[7].catalogNbr+" "+oChem.data.classes[7].titleLong);
+	 }
+            
+  
+    }})
+    }})
+    }})
+    }});//end of questionnaire
+
+/*
+	$( "#test" ).css( "border", "3px solid white" );
+    
+    $.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=MATH", success: function(result){
+        var math = result;
+        console.log(result);
+        
+    $.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=PHYS", success: function(result){
+        var phys = result;
+        console.log(result);
+        
+    $.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=CHEM", success: function(result){
+        var chem = result;
+        console.log(result);
+        
+    $.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=CS", success: function(result){
+        var cs = result;
+        console.log(result);
+        
+    $.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=ENGRI", success: function(result){
+        var engri = result;
+        console.log(result);
+  */   
+
+           
+  /**  }})
+    }})
+    }})
+    }})
+    }});**/
+    
+
+
+}//end of function
+
   function makeUL(array) {
     // Create the list element:
     var list = document.createElement('ul');
@@ -42,66 +172,4 @@ document.getElementById("que3").value = "";
 
     // Finally, return the constructed list:
     return list;
-}
-
-
-// Add the contents of options[0] to #foo:
-document.getElementById('printOut').appendChild(makeUL(arrayMathClass));
-console.log(que1);
-switch(que1){
-
-          case("Mechanical Enginneer"):
-          case("Electrical and Computer Engineer"):
-          case("Applied and Engineering Physics"):
-             //All First Year Engineers
-              for(i = 0; i<41; i++){
-                if (math.data.classes[i].titleLong.search("for Engineers") > -1){ 
-                console.log(math.data.classes[i].subject)
-                console.log(math.data.classes[i].catalogNbr)
-                  }
-              }
-
-                console.log(chem.data.classes[6].subject)
-                console.log(chem.data.classes[6].catalogNbr)
-                break;
-      }
-    
-   /** $.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=PHYS", success: function(result){
-        var phys = result;
-        console.log(result);
-        
-    $.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=CHEM", success: function(result){
-        var chem = result;
-        console.log(result);
-        
-    $.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=CS", success: function(result){
-        var cs = result;
-        console.log(result);
-       
-    $.ajax({url: "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA16&subject=ENGRI", success: function(result){
-        var engri = result;
-        console.log(result);
-      switch(que1){
-
-          case("Mechanical Enginneer"):
-          case("Electrical and Computer Engineer"):
-          case("Applied and Engineering Physics"):
-             //All First Year Engineers
-              for(i = 0; i<41; i++){
-                if (math.data.classes[i].titleLong.search("for Engineers") > -1){ 
-                console.log(math.data.classes[i].subject)
-                console.log(math.data.classes[i].catalogNbr)
-                  }
-              }
-
-                console.log(chem.data.classes[6].subject)
-                console.log(chem.data.classes[6].catalogNbr)
-                break;
-      }
-    }})
-    }})
-    }})
-    }})
-    **/}});	
-
 }
